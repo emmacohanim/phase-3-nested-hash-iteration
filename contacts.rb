94e92f96d91e9e89caaf177c35a6901d1b69c977
Emma Cohanim <emmacohanim@gmail.com>
2022-09-01 08:46:27 -0700

    commit tree e9fa56c5ee128721a51c2340f3ed094fc964885f

--- contacts.rb
+++ contacts.rb
@@ -17,8 +17,23 @@ def contacts
   }
 end
 
+# iterates over contacts hash and remove strawberry when you reach :favorite_ice_cream_flavors
 def remove_strawberry(contacts)
+# 1. iterate over over top level of hash where keys = person & values = hash of details about person
   # your code here!
+  contacts.each do |person, contact_details_hash|
+# 2. iterate over second level if person is freddie
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        binding.pry
+# 3. locate element
+        if attribute == :favorite_ice_cream_flavors
+# 4. iterate through array of flavors and remove element that matches "strawberry"     
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+  end
+end
 end
 
 # print the output to the terminal for inspection
